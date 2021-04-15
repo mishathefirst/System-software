7cc9054a6d12c733d14f3514d6909b7eeb260fb6
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-04-15 06:04:22 +0300

    Add files via upload

--- LW1Prog/spoLW1/headers/constants.h
+++ LW1Prog/spoLW1/headers/constants.h
@@ -1,7 +1,7 @@
 #ifndef LABSPO1_CONSTANTS_H
 #define LABSPO1_CONSTANTS_H
 
-#define home_path "/home/avu"
+#define home_path "/home/misha"
 #define sys_inodes_count 10
 #define magic_num 0xEF53
 #define default_block_size 1024
